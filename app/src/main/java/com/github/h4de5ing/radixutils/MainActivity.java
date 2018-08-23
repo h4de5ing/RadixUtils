3828362431d3c8620add47c7a7aa0567ffd94c00
h4de5ing <moxi1992@gmail.com>
2018-08-23 11:43:58 +0800

    init

---
+++ app/src/main/java/com/github/h4de5ing/radixutils/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.h4de5ing.radixutils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
